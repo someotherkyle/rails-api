16bf9596844a844551366d001eb7eaf5ee08c7ca
someotherkyle <kylemclellan@gmail.com>
2019-07-25 11:38:43 -0400

    enabled bcrypt in Gemfile, added has_secure_password to User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :game, only: [:index, :show, :new, :create]
-    resources :user, only: [:new, :create]
+    resources :game, only: [:index, :create, :show]
+    resources :user, only: [:index, :create, :show]
 end
