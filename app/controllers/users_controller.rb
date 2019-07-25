0c509e47e841b1f925de409af869358647ef3d50
someotherkyle <kylemclellan@gmail.com>
2019-07-25 11:43:03 -0400

    updated games and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password)
+    end
 end
