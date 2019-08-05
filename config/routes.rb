d92c74f54e595c78093506bbd2acfbd480731355
someotherkyle <kylemclellan@gmail.com>
2019-08-05 12:57:54 -0400

    added faker, removed tiles model

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :game, only: [:index, :create, :show]
     resources :user, only: [:create, :show]
-    resources :tile, only: [:create, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do 
+  Game.create(score: Faker::Number.number(digits: 6))
+end
